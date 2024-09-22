059ea80dd2e62d1070dd20f406fc1cab89afbda6
Jake Bailey <asaxplayinghorse@gmail.com>
2024-09-22 14:07:26 -0700

    SymbolTable module complete, now to test

--- modules/symbol_table.py
+++ modules/symbol_table.py
@@ -15,4 +15,45 @@ class SymbolTable():
     """Stores and manipulates the symbol table for a Hack assembly program."""
 
     def __init__(self):
-        self.symbol_table = {}
+        # Initialize the symbol table with the predefined labels
+        self.symbol_table = {
+            "    SP":     0,
+            "   LCL":     1,
+            "   ARG":     2,
+            "  THIS":     3,
+            "  THAT":     4,
+            "    R0":     0,
+            "    R1":     1,
+            "    R2":     2,
+            "    R3":     3,
+            "    R4":     4,
+            "    R5":     5,
+            "    R6":     6,
+            "    R7":     7,
+            "    R8":     8,
+            "    R9":     9,
+            "   R10":    10,
+            "   R11":    11,
+            "   R12":    12,
+            "   R13":    13,
+            "   R14":    14,
+            "   R15":    15,
+            "SCREEN": 16384,
+            "   KBD": 24576,
+        }
+
+    def add_entry(self, label: str, addr: int):
+        if self.contains(label):
+            print("Error, label already exists.")
+            return
+
+        self.symbol_table[label] = addr
+
+    def contains(self, label):
+        return (label in self.symbol_table.keys())
+
+    def get_address(self, label):
+        if not self.contains(label):
+            print("Error, symbol not in table.")
+
+        return self.symbol_table[label]
